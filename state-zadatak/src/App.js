d3f8df4d35d49257d5ab7aae5df085920a5254ad
Nikola Kranjcec <kranjcecni@gmail.com>
2023-07-19 21:06:39 +0200

    state drugi zadatak

--- state-zadatak/src/App.js
+++ state-zadatak/src/App.js
@@ -1,43 +1,48 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Komponenta1, Komponenta2, Komponenta3, Button } from "./components";
 
-export default class App extends Component {
-  state = {
-    users: [
-      { name: "Boris", age: 28 },
-      { name: "Ana", age: 35 },
-      { name: "Bob", age: 55 },
-    ],
-    randomText: "Ovo je nasumični tekst.",
-  };
+function App() {
+  let users = [
+    { name: "Boris", age: 28 },
+    { name: "Ana", age: 35 },
+    { name: "Bob", age: 55 },
+  ];
 
-  hendleClick = () => {
-    let newUsers = this.state.users;
+  const randomText = "random tekst";
+  const [stateUsers, setUsers] = useState(users);
+
+  function hendleClick() {
+    let newUsers = [...stateUsers];
     newUsers[0].age = newUsers[0].age + 1;
-    this.setState({
-      users: newUsers,
-    });
-  };
-  render() {
-    return (
-      <div>
-        <Komponenta1
-          name={this.state.users[0].name}
-          age={this.state.users[0].age}
-        />
 
-        <Button text="Dodaj godine" hendleClick={this.hendleClick} />
-        <Komponenta2
-          name={this.state.users[1].name}
-          age={this.state.users[1].age}
-        />
-        <Komponenta3
-          name={this.state.users[2].name}
-          age={this.state.users[2].age}
-        >
-          <p>{this.state.randomText}</p>
-        </Komponenta3>
-      </div>
-    );
+    console.log(newUsers);
+    setUsers(newUsers);
   }
+
+  function changeName(newName) {
+    console.log(newName);
+    let newUsers = [...stateUsers];
+    newUsers[0].name = newName;
+
+    console.log(newUsers);
+    setUsers(newUsers);
+  }
+
+  return (
+    <div className="App">
+      <Komponenta1 name={stateUsers[0].name} age={stateUsers[0].age} />
+
+      <Button text="Dodaj godine" hendleClick={hendleClick} />
+      <input
+        value={stateUsers[0].name}
+        onChange={(e) => changeName(e.target.value)}
+      ></input>
+      <Komponenta2 name={stateUsers[1].name} age={stateUsers[1].age} />
+      <Komponenta3 name={stateUsers[2].name} age={stateUsers[2].age}>
+        <p>{randomText}</p>
+      </Komponenta3>
+    </div>
+  );
 }
+
+export default App;
